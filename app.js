c2a3eefb0e663b7065aa45ab496f329556c48a3c
Muhammad Muzammil <126906348+muhammad-muzammil-123@users.noreply.github.com>
2023-09-19 11:22:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,157 @@
+// #*#**#*#***#*#*#*#**#*#*#*#*# CHAPTER (14) TO (16) #*#*#*#*#*#*#*#*#*#*#*#*#
+// ######################### QUESTION NO:1 #################
+// var students = [];
+// students[0] = "muhammad";
+// students [1]= "muzammil";
+// students[2] = "hameed";
+// console.log(students)
+
+// ################ QUESTION NO:2 #############
+
+
+// var studentsNamesInFuture = [];
+// studentsNamesInFuture.push("muhammad");
+// studentsNamesInFuture.push("muzammil");
+// console.log(studentsNamesInFuture);
+
+// #################### QUESTION NO:3 ###################
+
+// var arr = ["muzammil","hameed","hasssaan"];
+// console.log(arr);
+
+// ################## QUESTION NO: 4  ##################
+// var numbersArr = [1,2,3,4,5,6,7];
+// console.log(numbersArr[3]);
+
+// #################### QUESTION NO: 5 ##################
+// var mixedArr = ["muzammil",4,6,true,"muhammad",false,9];
+// console.log(mixedArr);
+
+// ###################### QUESTION NO: 6 ###################
+
+// var studentsName = ["muhammad","muzammil","hameed"];
+// var studentsScore = [320,350,400];
+// var studentsPercentage = [320/500*100 + "%" , 350/500*100 + "%",400/500*100 + "%"]
+// document.write(studentsName[0] +" " + studentsScore[0]  +" " +  studentsPercentage[0] + "<br>");
+// document.write(studentsName[1] +" " + studentsScore[1] +" " + studentsPercentage[1] + "<br>")
+// document.write(studentsName[2] +" " + studentsScore[2] +" " + studentsPercentage[2])
+
+// ############## QUESTION NO: 7 #################
+                // UNSHIFT METHOD
+// var userColor = prompt("Enter your favourite color");
+// var colorArr = ["red","blue","green","black"];
+// console.log(colorArr.unshift(userColor));
+// console.log(colorArr);
+                    //  PUSH METHOD
+// var userColor = prompt("Enter your favourite color");
+// var colorArr = ["red","blue","green","black"];
+// console.log(colorArr.push(userColor));
+// console.log(colorArr);
+            // UNSHIFT METHOD
+// var userColor = prompt("Enter your favourite color");
+//  var colorArr = ["red","blue","green","black"];
+// console.log(colorArr.unshift("white","yellow"));
+// console.log(colorArr);
+ 
+            //    SHIFT METHOD
+//  var colorArr = ["red","blue","green","black"]
+// console.log(colorArr.shift());
+// console.log(colorArr);
+
+                //   POP METHOD
+                
+// var colorArr = ["red","blue","green","black"];
+// console.log(colorArr.pop());
+// console.log(colorArr);
+                    
+                //  SPLICE METHOD
+//    var userColor = prompt("ENTER YOUR FAVOURITE COLOR");
+//    var userIndex = +prompt("enter index");
+//    var colorArr = ["red","blue","green","black"];
+// console.log(colorArr.splice(userIndex,2 ,userColor));
+// console.log(colorArr);
+    
+// ################### QUESTION NO: 7 ################
+
+        // var arr = [320,450,780,210,130];
+        // console.log(arr)
+        // console.log(arr.sort());
+        // // console.log(arr);
+        // console.log(arr.reverse());     
+
+        // ############# QUESTION NO: 8 ################
+
+// var arr = ["lahore","islamabad","peshawar","quetta","multan"];
+// console.log(arr);
+// console.log(arr.slice(2,4))
+
+// ###################### QUESTION NO:9 ##########################
+// var arr = ["this","is","my","cat"];;
+// console.log(arr)
+// console.log(arr.join( "_"));
+
+// #################### QUESTION NO : 10 ########################
+
+// var mobilePhone = ["nokia","motorola","samsung","iphone"]
+// document.write("<select>" +  "<option>" + mobilePhone[0] + "</option>" + "<option>" + mobilePhone[1] + "</option>" + "<option>" + mobilePhone[2] + "</option>" + "<option>" + mobilePhone[3] + "</option>"+ "</select>" + "<br>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
